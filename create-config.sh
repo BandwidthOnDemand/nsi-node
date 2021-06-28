9d631e66456f943cd30c14e19a4c5cc1dd84face
Hans Trompert <hans.trompert@surf.nl>
2021-06-28 16:12:59 +0200

    per app config files + logging + use zsh instead of bash

--- create-config.sh
+++ create-config.sh
@@ -1,13 +1,42 @@
-#!/bin/bash
+#!/usr/bin/env zsh
 
-getCertificateCommonName()
-{
+declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
+script_logging_level="INFO"
+
+while getopts ":d" opt
+do
+    case ${opt} in
+        d )
+            script_logging_level="DEBUG"
+            ;;
+        \? )
+            echo "Usage: `basename $0` [-d]"
+            exit 1
+            ;;
+    esac
+done
+shift $((OPTIND -1))
+
+log() {
+    local log_priority=$1
+    local log_message=${@:2}
+
+    #check if level exists
+    [[ ${levels[$log_priority]} ]] || return 1
+
+    #check if level is enough
+    (( ${levels[$log_priority]} < ${levels[$script_logging_level]} )) && return 2
+
+    #log here
+    printf "%-5s %s\n" "${log_priority}" "${log_message}"
+}
+
+getCertificateCommonName() {
     openssl x509 -noout -subject -in "$1" |
         sed 's/^.*\/CN=\(.*\)$/\1/'
 }
 
-createSpki()
-{
+createSpki() {
     openssl x509 -noout -pubkey -in "$1" |
         openssl pkey -pubin -outform DER |
         openssl dgst -sha256 -binary |
@@ -17,32 +46,38 @@ createSpki()
 configBaseFolder="config"
 for app in nsi-dds nsi-safnari nsi-pce
 do
-    configFolder=${configBaseFolder}/${app}
+    log INFO "======================"
+	log INFO "`echo ${app} | tr a-z A-Z`"
+	log INFO "======================"
+    configFolder="${configBaseFolder}/${app}"
+    runtimeConfigFolder="charts/${app}/config"
+    test ! -d "${runtimeConfigFolder}" && mkdir "${runtimeConfigFolder}" && log DEBUG "created runtime config folder ${runtimeConfigFolder}"
     if ! test -d "${configFolder}"
     then
-        echo "ERROR: cannot find per application configuration folder(s) in config folder"
+        log ERROR "ERROR: cannot find per application configuration folder(s) in config folder"
         exit 1
     fi
+    #
+    # create truststore
+    #
+    CNs=()
     truststore="charts/${app}/config/${app}-truststore.jks"
-    echo ======================
-	echo ${app} truststore
-	echo ======================
-	test -f "${truststore}" && rm "${truststore}" && echo removed old "${truststore}"
-	for certificate in ${configFolder}/certificates/trust/*.crt ${configFolder}/certificates/trust/*.chain
+	test -f "${truststore}" && rm "${truststore}" && log DEBUG "removed old ${truststore}"
+    find "${configFolder}/certificates/trust" \( -name '*.crt' -o -name '*.chain' \) -print | while read certificate
 	do
-		if ! test -f "${certificate}"
-		then 
-            echo "WARNING: does ${app} not trust any peers???"
-			break
-		fi
 		commonName=`getCertificateCommonName "${certificate}"`
-		echo "adding certificate for ${commonName}"
-		keytool -import -destkeystore "${truststore}" -alias "${commonName}" -storepass secret -noprompt -file "${certificate}"
+        if (($CNs[(I)$commonName]))
+        then
+            log DEBUG "already in truststore: ${commonName}"
+            continue
+        fi
+        CNs+="${commonName}"
+		log INFO "adding certificate to truststore: ${commonName}"
+		keytool -import -destkeystore "${truststore}" -alias "${commonName}" -storepass secret -noprompt -file "${certificate}" 2>/dev/null || log ERROR "could not import certificate"
 	done
-
-	echo ======================
-	echo ${app} keystore
-	echo ======================
+    #
+    # create keystore
+    #
     keystore="charts/${app}/config/${app}-keystore.jks"
     p12tmpkeystore="`mktemp`"
     certificate="`echo ${configFolder}/certificates/key/*.crt`"
@@ -50,63 +85,87 @@ do
     chain="`echo ${configFolder}/certificates/key/*.chain`"
 	if test ! -f "${certificate}" -o ! -f "${key}" -o ! "${chain}"
 	then 
-		echo "ERROR: cannot find complete set of key, certifcate and chain for ${app}"
+		log ERROR "cannot find complete set of key, certifcate and chain for ${app}"
         exit 1
 	fi
-	test -f "${keystore}" && rm "${keystore}" && echo removed old "${keystore}"
+	test -f "${keystore}" && rm "${keystore}" && log "DEBUG removed old ${keystore}"
     commonName=`getCertificateCommonName "${certificate}"`
-	echo creating p12 keystore
-	echo adding ${commonName} to p12 keystore
+	log DEBUG "creating p12 keystore"
+	log INFO "adding certificate to keystore: ${commonName}"
 	openssl pkcs12 -export -name "${commonName}" -in "${certificate}" -inkey "${key}" -out "${p12tmpkeystore}" -CAfile "${chain}" -password pass:secret -chain
-	echo converting pkcs12 keystore to jks
-	keytool -importkeystore -destkeystore "${keystore}" -srckeystore "${p12tmpkeystore}" -srcstoretype pkcs12 -srcstorepass secret -storepass secret -alias "${commonName}" -noprompt
-	test -f "${p12tmpkeystore}" && rm "${p12tmpkeystore}" && echo removed "${p12tmpkeystore}"
+	log DEBUG "converting pkcs12 keystore to jks"
+	keytool -importkeystore -destkeystore "${keystore}" -srckeystore "${p12tmpkeystore}" -srcstoretype pkcs12 -srcstorepass secret -storepass secret -alias "${commonName}" -noprompt 2>/dev/null ||
+        log ERROR "could not covert keystore from p12 to jks"
+	test -f "${p12tmpkeystore}" && rm "${p12tmpkeystore}" && log DEBUG "removed ${p12tmpkeystore}"
+    #
+    # copying config files
+    #
+	log INFO "installing config file(s)"
+    configFiles=()
+    case ${app} in
+        nsi-dds )
+            configFiles+=dds.xml
+            configFiles+=log4j.xml
+            configFiles+=logging.properties
+            ;;
+        nsi-safnari )
+            configFiles+=config-overrides.conf
+            ;;
+        nsi-pce )
+            configFiles+=log4j.xml
+            configFiles+=logging.properties
+            configFiles+=topology-dds.xml
+            configFiles+=beans.xml
+            configFiles+=http.json
+            ;;
+    esac
+    for file in ${configFiles}
+    do
+        cp -p ${configFolder}/templates/${file} ${runtimeConfigFolder} && log DEBUG "installed" ${file}
+    done
 done
 
-echo ======================
-echo envoy configuration
-echo ======================
+log INFO "======================"
+log INFO ENVOY
+log INFO "======================"
 envoyCaChain="charts/nsi-envoy/config/nsi-envoy-ca-chain.pem"
-test -f "${envoyCaChain}"  && rm "${envoyCaChain}" && echo removed old "${envoyCaChain}"
-for certificate in config/*/certificates/trust/*.chain
+runtimeConfigFolder="charts/nsi-envoy/config"
+test ! -d "${runtimeConfigFolder}" && mkdir "${runtimeConfigFolder}" && log DEBUG "created runtime config folder ${runtimeConfigFolder}"
+test -f "${envoyCaChain}"  && rm "${envoyCaChain}" && log DEBUG "removed old ${envoyCaChain}"
+find "config" -name '*.chain' -regex '.*/trust/[^/]*\.chain' | while read certificate
 do
     if ! test -f "${certificate}"
     then
-        echo "WARNING: could not find any CA to trust!"
+        log WARN "could not find any CA to trust!"
         break
     fi
-	echo adding CA to envoy chain: `getCertificateCommonName "${certificate}"`
+	log INFO "adding CA to envoy chain:" `getCertificateCommonName "${certificate}"`
 	openssl x509 -noout -subject -issuer -in "${certificate}" >>"${envoyCaChain}"
 	cat "${certificate}" >>"${envoyCaChain}"
 done
 envoyConfig="charts/nsi-envoy/config/envoy.yaml"
-echo "creating envoy config ..."
-test -f "${envoyConfig}"  && rm "${envoyConfig}" && echo removed old "${envoyConfig}"
-echo "adding skeleton config ..."
+log DEBUG "creating envoy config"
+test -f "${envoyConfig}"  && rm "${envoyConfig}" && log DEBUG "removed old ${envoyConfig}"
+log DEBUG "adding skeleton config ..."
 cat "config/nsi-envoy/templates/envoy-head.yaml" >>${envoyConfig}
 for app in nsi-dds nsi-safnari
 do
-    echo "copying ${app} key and chain to envoy config folder ..."
+    log INFO "copying ${app} key and chain to envoy config folder"
     cat config/${app}/certificates/key/*.key >charts/nsi-envoy/config/${app}.key
     cat config/${app}/certificates/key/*.chain >charts/nsi-envoy/config/${app}.chain
     cat "config/${app}/templates/envoy-filter_chain_match.yaml" >>${envoyConfig}
     echo "              verify_certificate_spki:" >>${envoyConfig}
-    for certificate in config/${app}/certificates/trust/*.crt
+    find "config/${app}/certificates/trust" -name '*.crt' -print | while read certificate
     do
-        if ! test -f "${certificate}"
-        then
-            echo "WARNING: does ${app} not trust any peers???"
-            break
-        fi
         spki=`createSpki "${certificate}"`
         commonName=`getCertificateCommonName "${certificate}"`
-        echo "adding ${commonName} SPKI to ${app} envoy config"
+        log INFO "adding SPKI to ${app} envoy config: ${commonName}"
         echo "              - \"${spki}\" # ${commonName}" >>${envoyConfig}
     done
 done
 echo "  clusters:" >>${envoyConfig}
 for app in nsi-dds nsi-safnari
 do
-    echo "adding ${app} cluster config ..."
+    log DEBUG "adding ${app} cluster config ..."
     cat "config/${app}/templates/envoy-cluster.yaml" >>${envoyConfig}
 done
