9e17046e9cd2707bff7632a30db6a90fd7e60855
Hans Trompert <hans.trompert@surf.nl>
2021-07-07 18:20:10 +0200

    configurable config folder option

--- create-config.sh
+++ create-config.sh
@@ -2,20 +2,8 @@
 
 declare -A levels=([DEBUG]=0 [INFO]=1 [WARN]=2 [ERROR]=3)
 script_logging_level="INFO"
+configBaseFolder="config"
 
-while getopts ":d" opt
-do
-    case ${opt} in
-        d )
-            script_logging_level="DEBUG"
-            ;;
-        \? )
-            echo "Usage: `basename $0` [-d]"
-            exit 1
-            ;;
-    esac
-done
-shift $((OPTIND -1))
 
 log() {
     local log_priority=$1
@@ -105,144 +93,181 @@ createSpki() {
 #
 # get and untar Helm charts
 #
-helm dependency update || { log ERROR helm dependency update failed; exit 1 }
-(
-    cd charts
-    for chart in *.tgz
+getHelmCharts()
+{
+    test ! -f Chart.yaml && log ERROR cannot find Chart.yaml && exit 1
+    test ! -f values.yaml && log ERROR cannot find values.yaml && exit 1
+    helm dependency update || { log ERROR helm dependency update failed; exit 1 }
+    (
+        cd charts
+        for chart in *.tgz
+        do
+            tar -xf "$chart" && rm -f "$chart"
+        done
+    )
+    for app in nsi-safnari nsi-pce nsi-dds nsi-envoy
     do
-        tar -xf "$chart" && rm -f "$chart"
+        if appEnabled ${app} && test ! -d "charts/${app}/config"
+        then
+            mkdir "charts/${app}/config" && log DEBUG "created folder ${app}/config"
+        fi
     done
-)
-for app in nsi-safnari nsi-pce nsi-dds nsi-envoy
-do
-    if appEnabled ${app}
-    then
-        mkdir "charts/${app}/config" && log DEBUG "created folder ${app}/config"
-    fi
-done
+}
 
 #
 # create per app config
 #
-configBaseFolder="config"
-checkConfigFolders
-for app in nsi-dds nsi-safnari nsi-pce
-do
-    log INFO "======================"
-	log INFO "`echo ${app} | tr a-z A-Z`"
-	log INFO "======================"
-    configFolder="${configBaseFolder}/${app}"
-    runtimeConfigFolder="charts/${app}/config"
-    #
-    # create truststore
-    #
-    CNs=()
-    truststore="charts/${app}/config/${app}-truststore.jks"
-	ifExistExecute DEBUG "${truststore}" 'rm ${file} && log DEBUG removed old ${file}'
-    find "${configFolder}/certificates/trust" \( -name '*.crt' -o -name '*.chain' \) -print | while read certificate
-	do
-		commonName=`getCertificateCommonName "${certificate}"`
-        if (($CNs[(I)$commonName]))
-        then
-            log DEBUG "already in truststore: ${commonName}"
-            continue
-        fi
-        CNs+="${commonName}"
-		log INFO "adding certificate to truststore: ${commonName}"
-		keytool -import -destkeystore "${truststore}" -alias "${commonName}" -storepass secret -noprompt -file "${certificate}" 2>/dev/null || log ERROR "could not import certificate"
-	done
-    #
-    # create keystore
-    #
-    keystore="charts/${app}/config/${app}-keystore.jks"
-    p12tmpkeystore="`mktemp`"
-    certificate="`echo ${configFolder}/certificates/key/*.crt`"
-    key="`echo ${configFolder}/certificates/key/*.key`"
-    chain="`echo ${configFolder}/certificates/key/*.chain`"
-	if test ! -f "${certificate}" -o ! -f "${key}" -o ! "${chain}"
-	then 
-		log ERROR "cannot find complete set of key, certifcate and chain for ${app}"
-        exit 1
-	fi
-	ifExistExecute DEBUG "${keystore}" 'rm ${file} && log DEBUG removed old ${file}'
-    commonName=`getCertificateCommonName "${certificate}"`
-	log DEBUG "creating p12 keystore"
-	log INFO "adding certificate to keystore: ${commonName}"
-	openssl pkcs12 -export -name "${commonName}" -in "${certificate}" -inkey "${key}" -out "${p12tmpkeystore}" -CAfile "${chain}" -password pass:secret -chain
-	log DEBUG "converting pkcs12 keystore to jks"
-	keytool -importkeystore -destkeystore "${keystore}" -srckeystore "${p12tmpkeystore}" -srcstoretype pkcs12 -srcstorepass secret -storepass secret -alias "${commonName}" -noprompt 2>/dev/null ||
-        log ERROR "could not covert keystore from p12 to jks"
-	ifExistExecute DEBUG "${p12tmpkeystore}" 'rm ${file} && log DEBUG removed ${file}'
-    #
-    # copying config files
-    #
-	log INFO "installing config file(s)"
-    configFiles=()
-    case ${app} in
-        nsi-dds )
-            configFiles+=dds.xml
-            configFiles+=log4j.xml
-            configFiles+=logging.properties
-            ;;
-        nsi-safnari )
-            configFiles+=config-overrides.conf
-            ;;
-        nsi-pce )
-            configFiles+=log4j.xml
-            configFiles+=logging.properties
-            configFiles+=topology-dds.xml
-            configFiles+=beans.xml
-            configFiles+=http.json
-            ;;
-    esac
-    for file in ${configFiles}
+createAppConfig() {
+    for app in nsi-dds nsi-safnari nsi-pce
     do
-	    ifExistExecute ERROR ""${configFolder}/templates/${file} "cp -p \${file} ${runtimeConfigFolder} && log DEBUG installed \${file}"
+        log INFO "======================"
+        log INFO "`echo ${app} | tr a-z A-Z`"
+        log INFO "======================"
+        configFolder="${configBaseFolder}/${app}"
+        runtimeConfigFolder="charts/${app}/config"
+        #
+        # create truststore
+        #
+        CNs=()
+        truststore="charts/${app}/config/${app}-truststore.jks"
+        ifExistExecute DEBUG "${truststore}" 'rm ${file} && log DEBUG removed old ${file}'
+        find "${configFolder}/certificates/trust" \( -name '*.crt' -o -name '*.chain' \) -print | while read certificate
+        do
+            commonName=`getCertificateCommonName "${certificate}"`
+            if (($CNs[(I)$commonName]))
+            then
+                log DEBUG "already in truststore: ${commonName}"
+                continue
+            fi
+            CNs+="${commonName}"
+            log INFO "adding certificate to truststore: ${commonName}"
+            keytool -import -destkeystore "${truststore}" -alias "${commonName}" -storepass secret -noprompt -file "${certificate}" 2>/dev/null || log ERROR "could not import certificate"
+        done
+        #
+        # create keystore
+        #
+        keystore="charts/${app}/config/${app}-keystore.jks"
+        p12tmpkeystore="`mktemp`"
+        certificate="`echo ${configFolder}/certificates/key/*.crt`"
+        key="`echo ${configFolder}/certificates/key/*.key`"
+        chain="`echo ${configFolder}/certificates/key/*.chain`"
+        if test ! -f "${certificate}" -o ! -f "${key}" -o ! "${chain}"
+        then 
+            log ERROR "cannot find complete set of key, certifcate and chain for ${app}"
+            exit 1
+        fi
+        ifExistExecute DEBUG "${keystore}" 'rm ${file} && log DEBUG removed old ${file}'
+        commonName=`getCertificateCommonName "${certificate}"`
+        log DEBUG "creating p12 keystore"
+        log INFO "adding certificate to keystore: ${commonName}"
+        openssl pkcs12 -export -name "${commonName}" -in "${certificate}" -inkey "${key}" -out "${p12tmpkeystore}" -CAfile "${chain}" -password pass:secret -chain
+        log DEBUG "converting pkcs12 keystore to jks"
+        keytool -importkeystore -destkeystore "${keystore}" -srckeystore "${p12tmpkeystore}" -srcstoretype pkcs12 -srcstorepass secret -storepass secret -alias "${commonName}" -noprompt 2>/dev/null ||
+            log ERROR "could not covert keystore from p12 to jks"
+        ifExistExecute DEBUG "${p12tmpkeystore}" 'rm ${file} && log DEBUG removed ${file}'
+        #
+        # copying config files
+        #
+        log INFO "installing config file(s)"
+        configFiles=()
+        case ${app} in
+            nsi-dds )
+                configFiles+=dds.xml
+                configFiles+=log4j.xml
+                configFiles+=logging.properties
+                ;;
+            nsi-safnari )
+                configFiles+=config-overrides.conf
+                ;;
+            nsi-pce )
+                configFiles+=log4j.xml
+                configFiles+=logging.properties
+                configFiles+=topology-dds.xml
+                configFiles+=beans.xml
+                configFiles+=http.json
+                ;;
+        esac
+        for file in ${configFiles}
+        do
+            ifExistExecute ERROR ""${configFolder}/templates/${file} "cp -p \${file} ${runtimeConfigFolder} && log DEBUG installed \${file}"
+        done
     done
-done
+}
 #
 # create envoy config
 #
-log INFO "======================"
-log INFO ENVOY
-log INFO "======================"
-envoyCaChain="charts/nsi-envoy/config/nsi-envoy-ca-chain.pem"
-runtimeConfigFolder="charts/nsi-envoy/config"
-ifExistExecute DEBUG "${envoyCaChain}" 'rm ${file} && log DEBUG removed old ${file}'
-find "config" -name '*.chain' -regex '.*/trust/[^/]*\.chain' | while read certificate
-do
-    if ! test -f "${certificate}"
-    then
-        log WARN "could not find any CA to trust!"
-        break
-    fi
-	log INFO "adding CA to envoy chain:" `getCertificateCommonName "${certificate}"`
-	openssl x509 -noout -subject -issuer -in "${certificate}" >>"${envoyCaChain}"
-	cat "${certificate}" >>"${envoyCaChain}"
-done
-envoyConfig="charts/nsi-envoy/config/envoy.yaml"
-log DEBUG "creating envoy config"
-ifExistExecute DEBUG "${envoyConfig}" 'rm ${file} && log DEBUG removed old ${file}'
-log DEBUG "adding skeleton config ..."
-ifExistExecute ERROR "config/nsi-envoy/templates/envoy-head.yaml" "cat \${file} >>${envoyConfig}"
-for app in nsi-dds nsi-safnari
-do
-    log INFO "copying ${app} key and chain to envoy config folder"
-    cat config/${app}/certificates/key/*.key >charts/nsi-envoy/config/${app}.key
-    cat config/${app}/certificates/key/*.chain >charts/nsi-envoy/config/${app}.chain
-    ifExistExecute ERROR "config/${app}/templates/envoy-filter_chain_match.yaml" "cat \${file} >>${envoyConfig}"
-    echo "              verify_certificate_spki:" >>${envoyConfig}
-    find "config/${app}/certificates/trust" -name '*.crt' -print | while read certificate
+createEnvoyConfig() {
+    log INFO "======================"
+    log INFO ENVOY
+    log INFO "======================"
+    envoyCaChain="charts/nsi-envoy/config/nsi-envoy-ca-chain.pem"
+    runtimeConfigFolder="charts/nsi-envoy/config"
+    ifExistExecute DEBUG "${envoyCaChain}" 'rm ${file} && log DEBUG removed old ${file}'
+    find "config" -name '*.chain' -regex '.*/trust/[^/]*\.chain' | while read certificate
     do
-        spki=`createSpki "${certificate}"`
-        commonName=`getCertificateCommonName "${certificate}"`
-        log INFO "adding SPKI to ${app} envoy config: ${commonName}"
-        echo "              - \"${spki}\" # ${commonName}" >>${envoyConfig}
+        if ! test -f "${certificate}"
+        then
+            log WARN "could not find any CA to trust!"
+            break
+        fi
+        log INFO "adding CA to envoy chain:" `getCertificateCommonName "${certificate}"`
+        openssl x509 -noout -subject -issuer -in "${certificate}" >>"${envoyCaChain}"
+        cat "${certificate}" >>"${envoyCaChain}"
     done
-done
-echo "  clusters:" >>${envoyConfig}
-for app in nsi-dds nsi-safnari
+    envoyConfig="charts/nsi-envoy/config/envoy.yaml"
+    log DEBUG "creating envoy config"
+    ifExistExecute DEBUG "${envoyConfig}" 'rm ${file} && log DEBUG removed old ${file}'
+    log DEBUG "adding skeleton config ..."
+    ifExistExecute ERROR "${configBaseFolder}/nsi-envoy/templates/envoy-head.yaml" "cat \${file} >>${envoyConfig}"
+    for app in nsi-dds nsi-safnari
+    do
+        log INFO "copying ${app} key and chain to envoy config folder"
+        cat ${configBaseFolder}/${app}/certificates/key/*.key >charts/nsi-envoy/config/${app}.key
+        cat ${configBaseFolder}/${app}/certificates/key/*.chain >charts/nsi-envoy/config/${app}.chain
+        ifExistExecute ERROR "${configBaseFolder}/${app}/templates/envoy-filter_chain_match.yaml" "cat \${file} >>${envoyConfig}"
+        echo "              verify_certificate_spki:" >>${envoyConfig}
+        find "${configBaseFolder}/${app}/certificates/trust" -name '*.crt' -print | while read certificate
+        do
+            spki=`createSpki "${certificate}"`
+            commonName=`getCertificateCommonName "${certificate}"`
+            log INFO "adding SPKI to ${app} envoy config: ${commonName}"
+            echo "              - \"${spki}\" # ${commonName}" >>${envoyConfig}
+        done
+    done
+    echo "  clusters:" >>${envoyConfig}
+    for app in nsi-dds nsi-safnari
+    do
+        log DEBUG "adding ${app} cluster config ..."
+        ifExistExecute ERROR "${configBaseFolder}/${app}/templates/envoy-cluster.yaml" "cat \${file} >>${envoyConfig}"
+    done
+}
+
+#
+# main
+#
+while getopts ":dc:" opt
 do
-    log DEBUG "adding ${app} cluster config ..."
-    ifExistExecute ERROR "config/${app}/templates/envoy-cluster.yaml" "cat \${file} >>${envoyConfig}"
+    case ${opt} in
+        d )
+            script_logging_level="DEBUG"
+            ;;
+        c )
+            configBaseFolder="${OPTARG}"
+            log DEBUG "using ${configBaseFolder} as configuration folder"
+            ;;
+        ":" )
+            log ERROR "option -${OPTARG} requires an argument"
+            exit 1
+            ;;
+        \? )
+            echo "Usage: `basename $0` [-d] [-c <configfolder>]"
+            exit 1
+            ;;
+    esac
 done
+shift $((OPTIND -1))
+
+checkConfigFolders
+getHelmCharts
+createAppConfig
+createEnvoyConfig
